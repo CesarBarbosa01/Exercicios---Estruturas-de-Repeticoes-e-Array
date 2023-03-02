5e6649c632b1d5f575ee29d0b3da831e5815fe83
César Barbosa <edinaldocesar14@gmail.com>
2023-03-02 12:04:17 -0300

    tentativas

---
+++ exercicios/ex01.js
@@ -0,0 +1,16 @@
+/*
+Crie um programa que leia 6 valores inteiros de um array e, em seguida,
+mostre na tela os valores lidos na ordem inversa.
+*/
+
+let numeros = [1,6,10,14,20,21]
+let n1 = numeros[0]
+let n2 = numeros[1]
+let n3 = numeros[2]
+let n4 = numeros[3]
+let n5 = numeros[4]
+let n6 = numeros[5]
+let ordem = [n6, n5, n4, n3, n2, n1]
+console.log(ordem)
+
+    
